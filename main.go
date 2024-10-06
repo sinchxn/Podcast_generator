356f2b31b3b421d5bd0fba32abdaa6b0cd9eb10a
sinchxn <54508546+sinchxn@users.noreply.github.com>
2024-10-06 03:17:32 -0400

    rate change

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func main() {
 		}
 
 		tweets := []Tweet{}
-		for tweet := range scraper.SearchTweets(context.Background(), query, 100) {
+		for tweet := range scraper.SearchTweets(context.Background(), query, 2) {
 			if tweet.Error != nil {
 				log.Printf("Error: %v\n", tweet.Error)
 				continue
